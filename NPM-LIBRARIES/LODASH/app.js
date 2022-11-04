f606b53d231bf3e4dc81ae262250e1aecdcb415b
David Yakin <78755939+David-Yakin@users.noreply.github.com>
2022-11-04 13:29:38 +0200

    lodash

---
+++ NPM-LIBRARIES/LODASH/app.js
@@ -0,0 +1,42 @@
+const lodash = require("lodash");
+// const { add } = require("lodash");
+
+/** add **/
+// console.log(add(5, 65));
+
+/** random **/
+// const firstRandomNum = lodash.random(10, 100);
+// console.log(`first num: ${firstRandomNum}`);
+
+// const secondRandomNum = lodash.random(10, 100, true);
+// console.log(`line 11 - second num: ${secondRandomNum}`);
+
+// const thirdRandomNum = lodash.random(10, 100, false);
+// console.log(`line 12 - third num: ${thirdRandomNum}`);
+
+// const forthRandomNum = lodash.random(10, "hallo");
+// console.log(`line 13 - froth num: ${forthRandomNum}`);
+
+// const fifthRandomNum = lodash.random(10);
+// console.log(`line 14 - fifth num: ${fifthRandomNum}`);
+
+// const sixthRandomNum = lodash.random("hallo");
+// console.log(`line 15 - sixth num: ${sixthRandomNum}`);
+
+/** pick **/
+
+// const object = { a: 1, b: "2", c: 3 };
+
+// const pick = lodash.pick(object, ["a"]);
+// const pick = lodash.pick({ a: "blue", b: "yellow", c: "pink" }, ["a", "c"]);
+// console.log(pick);
+
+/** join **/
+// const array = ["a", "b", "c"];
+// const join = lodash.join(array, "-");
+// console.log(join);
+
+/* lodash exe */
+// const array = [1, 2, undefined, 3, 4, undefined];
+// let newArray = lodash.remove(array, n => n % 2 !== 0 && n !== undefined);
+// console.log(newArray);
